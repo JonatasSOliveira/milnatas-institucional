509337662a6337c2d2c3d3ed890d0d62c2d7a38f
Jonatas S. Oliveira <dev.jonatas.oliveira@gmail.com>
2023-08-12 17:16:36 +0000

    Updates

--- src/styles/colors.ts
+++
@@ -1,8 +0,0 @@
-export const PRIMARY_COLOR = "#a0d5e3";
-export const COMPLEMENTARY_COLOR_01 = "#6e9daa";
-export const COMPLEMENTARY_COLOR_02 = "#c4e5ed";
-export const HIGHLIGHT_COLOR_01 = "#f5b0bd";
-export const HIGHLIGHT_COLOR_02 = "#82c8b0";
-export const HIGHLIGHT_COLOR_03 = "#f9e19d";
-export const NEUTRAL_COLOR_01 = "#f5f5f5";
-export const NEUTRAL_COLOR_02 = "#333333";
